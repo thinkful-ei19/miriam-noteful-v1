7ca70f7271212152e0c0d903a3fa21bfb0154eca
Miriam Heller <miriam.heller@gmail.com>
2018-03-06 10:06:52 -0500

    completed challenge but still working on bonus

--- server.js
+++ server.js
@@ -1,8 +1,37 @@
 'use strict';
 
 // TEMP: Simple In-Memory Database
+
+const express = require('express');
+
 const data = require('./db/notes');
 
-console.log('hello world!');
+const app = express();
+const port = process.env.PORT || 8080;
 
-// INSERT EXPRESS APP CODE HERE...
+app.use(express.static('public'));
+
+// app.get('/api/notes', (req, res) => {
+//     if(req.query.searchTerm) {
+//         const { searchTerm } = req.query;
+//         ****const foundTerms = data.filter(note => note.includes(searchTerm));
+//         return res.json(foundTerms);
+//     }
+//     res.json(data);
+// });
+
+app.get('/api/notes/:id', (req, res) => {
+    const { id } = req.params;
+    const foundItem = data.find(item => item.id === Number(id));
+    if (foundItem) {
+        return res.json(foundItem);
+    }
+    return res.json({ error: 'no item found' });
+})
+
+// Listen for incoming connections
+app.listen(port, function () {
+    console.log(`Server listening on port ${port}`);
+}).on('error', err => {
+    console, error(err);
+})
